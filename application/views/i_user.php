14eb7a892e7198673195a099e8a993d88dfdb5f6
AbdulHayyiAlkatani <haialkatani@gmail.com>
2023-11-02 21:02:08 +0700

    first commit

---
+++ application/views/i_user.php
@@ -0,0 +1,16 @@
+<section>
+    <table border="1">
+        <?php foreach ($user as $u) { ?>
+        <tr>
+            <th>Nama</th>
+            <th>Alamat</th>
+            <th>Pekerjaan</th>
+        </tr>
+        <tr>
+            <td><?php echo $u -> nama?></td>
+            <td><?php echo $u -> alamat?></td>
+            <td><?php echo $u -> pekerjaan?></td>
+        </tr>
+        <?php } ?>
+    </table>
+</section>
